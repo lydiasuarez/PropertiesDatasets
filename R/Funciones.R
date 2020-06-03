70d4257bace5ab77b41f6bfeca7612a96c6089d0
meilwe <60190613+meilwe@users.noreply.github.com>
2020-06-03 18:35:29 +0200

    New Functions

--- R/Funciones.R
+++ R/Funciones.R
@@ -7,9 +7,9 @@ NULL
 #' @return Returns a vector of strings with the data sets included
 #' @export
 getDataSetList = function(){
-  dataset <- c("Zheisel")
-  cat("Available Datasets: ", "\n", dataset)
+  return(unique(basic_dataframe[,"Dataset"]))
 }
+
 
 #' Cell Types in Dataset
 #' @return Returns the cell types considered in the data set identified by the string in dataset
@@ -17,7 +17,7 @@ getDataSetList = function(){
 getCellTypesInDataSet <- function(dataset){
   if (dataset == "Zheisel"){
     cat("Cell types considered for this dataset are: ","\n")
-    gsub("ClassProbability_","",names(sympathetic$col.attrs)[grep("ClassProbability",names(sympathetic$col.attrs))])
+    return(unique(basic_dataframe[,"CellType"]))
   }
   else
     print("There are not available cell types for this dataset")
@@ -29,11 +29,24 @@ getCellTypesInDataSet <- function(dataset){
 getBrainregionsDataSet <- function(dataset){
   if (dataset == "Zheisel"){
     cat("Brain regions from", dataset,"are: ","\n")
-    gsub(".loom","",listloom)
+    return(unique(basic_dataframe[,"BrainRegion"]))
   }
   else
     print("There are not brain regions for this dataset")
 }
+
+#' Dataset Properties
+#' @return Returns a data frame of properties describing the dataset
+#' @export
+getDataSetInfo <- function(dataset){
+  if (dataset == "Zheisel"){
+    load("basic_dataframe")
+    return(basic_dataframe)
+  }
+  else
+    print("There are not information for this dataset")
+}
+
 
 #' Active Gene Methods
 #' @return Returns the available methods data for availability
@@ -42,7 +55,7 @@ getActiveGeneMethod <- function(dataset){
   methods= c("rawexpression", "variationcoefficient")
   if (dataset == "Zheisel"){
     cat("The available methods data for availability are: ","\n")
-    methods
+    return(unique(basic_dataframe[,"Method"]))
   }
   else
     print("There are not available methods for this dataset")
@@ -52,58 +65,9 @@ getActiveGeneMethod <- function(dataset){
 #' @return Returns the gene ids of genes active in the brain region, cell type, for a given calculus method.
 #' @export
 getActiveGenesInCellType <- function(dataset,brainregion,celltype,method){
-  if (dataset == "Zheisel"){
-    mymatrix = brainregion$matrix[,]
-    colnames(mymatrix) = brainregion$row.attrs$Gene[]
-    rownames(mymatrix) = brainregion$col.attrs$CellID[]
-    tgtmatrix = mymatrix[brainregion$col.attrs$Class[] == celltype,]
-    tissue = deparse(substitute(x))
-    saveRDS(tgtmatrix,paste0(mypath,tissue,celltype,".rds"))
-    expr.data = readRDS(paste0(mypath,tissue,celltype,".rds"))
-
-    if (method == "rawexpression"){
-      visibility = seq(0,0.1,0.01)
-      genes = colnames(expr.data)
-      pdata = unlist(lapply(visibility,function(x){
-        mask = colSums(expr.data > 0.5)  > (x * nrow(expr.data))
-        cat("Genes in this tissue for visibility",x,"are",sum(mask),"\n")
-        cat("First 5 visible genes for this threshold of visibility are",
-            paste0(colnames(expr.data)[mask][1:5],collapse=", "),"\n")
-        sum(mask)
-      }))
-      plot_rawexpression <- plot(x=visibility,
-           y=pdata,
-           main=paste0("Genes available for ",celltype),
-           type="l",
-           xlab="Visibility of the genes in cell number ratio")
-    }
-    if (method == "variationcoefficient"){
-      cv = function (x, na.rm = FALSE)
-      {
-        sd(x, na.rm = na.rm)/mean(x, na.rm = na.rm)
-      }
-      cvs = apply(expr.data,2,cv,na.rm=T)
-      plot_variationcoefficient <- plot(density(na.omit(cvs)),
-                 main=paste0("Variation coeff. for ",length(na.omit(cvs)),
-                             " mouse genes"),
-                 xlab="5000 probes selected (right of the vertical line)",
-                 ylab="variation coefficient")
-    }
-
-  }
+  datos_filtrados <-  datos[datos$Dataset == dataset & datos$BrainRegion == brainregion & datos$CellType == celltype & datos$Method == method,]
+  datos_filtrados <- datos_filtrados[rowSums(is.na(datos_filtrados)) != ncol(datos_filtrados),]
+  genes_activos <-grep("TRUE",datos_filtrados[,])
+  return(names(datos_filtrados)[genes_activos])
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
